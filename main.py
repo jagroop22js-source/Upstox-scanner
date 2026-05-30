6d59ae2f1805fb901deda92ceebd63283ac677c7
jagroop22js-source <jagroop22.js@gmail.com>
2026-05-30 18:25:04 +0530

    Implement error handling and request timeout
    Added error handling and timeout for the request.

--- main.py
+++ main.py
@@ -2,11 +2,20 @@ import requests
 
 url = "https://assets.upstox.com/market-quote/instruments/exchange/NSE.json"
 
-r = requests.get(url)
+try:
+    r = requests.get(url, timeout=20)
 
-print("STATUS:", r.status_code)
+    print("STATUS:", r.status_code)
 
-data = r.json()
+    if r.status_code == 200:
+        data = r.json()
 
-print("TOTAL RECORDS:", len(data))
-print(data[0])
+        print("TOTAL RECORDS:", len(data))
+        print(data[0])
+
+    else:
+        print("Response:")
+        print(r.text[:500])
+
+except Exception as e:
+    print("ERROR:", e)
